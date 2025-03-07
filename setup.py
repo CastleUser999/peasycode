a4e3015f39040329eb340cbfeffdbd4d9daff4d0
CastleUser999 <idiotfriendgroup@gmail.com>
2025-03-07 21:38:07 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as fh:
 	description = fh.read() 
 
 setuptools.setup( 
-	name="test-package", 
+	name="peasycode", 
 	version="0.0.1", 
-	author="GeeksforGeeks", 
-	author_email="contact@gfg.com", 
+	author="Bryce Garrido", 
+	author_email="idiotfriendgroup@gmail.com", 
 	packages=["test_package"], 
 	description="A sample test package", 
 	long_description=description, 
 	long_description_content_type="text/markdown", 
-	url="https://github.com/gituser/test-tackage", 
+	url="https://github.com/CastleUser999/peasycode/tree/main", 
 	license='MIT', 
 	python_requires='>=3.8', 
 	install_requires=[] 
