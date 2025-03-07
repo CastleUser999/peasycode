23b59b3ed515fb4d52be42f5df2d68132ca92557
CastleUser999 <idiotfriendgroup@gmail.com>
2025-03-07 21:45:31 +0800

    Create __init__.py

---
+++ peasycode/__init__.py
@@ -0,0 +1,8 @@
+def main() -> None:
+  print("Loading Peasycode...")
+  print("Choose a action!")
+  print("1. Add line")
+  print("2. Edit line")
+  print("3. Delete line")
+  print("4. Generate and run")
+  print("5. Exit __init__.py")
